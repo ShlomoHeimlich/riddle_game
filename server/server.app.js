6d69a8eb862a238e83a59caa4046b2840789bc4a
shlomo <s0556732899@gmail.com>
2025-07-10 20:29:48 +0300

    3

--- server/server.app.js
+++ server/server.app.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import router from './routes/router.js';
+import router_crud from './routes/router.js';
+// import router_plyer from
 const server=express()
 server.use(express.json())
-server.use(router);
+server.use(router_crud);
+// server.use("/plyer",router_plyer)
 server.listen(4545,()=>{console.log('4545');})
