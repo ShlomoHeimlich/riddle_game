ec96daa7d904cc687b10c8f4af78f552d69a4032
shlomo <s0556732899@gmail.com>
2025-07-11 13:51:06 +0300

    5

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,23 +1,30 @@
 import express from "express";
 const router = express.Router();
-
-import {ReadAllQuestions,deleted,Create_riddle,update_riddle} from "../controler/server.servises.js"
-
-
-router.get("/AllQuestions",async (req,res)=>{const answer = await ReadAllQuestions();
-  res.send(answer)})
+import { ReadAllQuestions, deleted, Create_riddle, update_riddle } from "../controler/controler.js"
 
 
-   router.delete("/delete/:id",async(req,res)=>{const answer = await deleted(req.params.id); 
-    res.send(answer) })
+router.get("/AllQuestions", async (req, res) => {
+  const answer = await ReadAllQuestions();
+  res.send(answer)
+})
 
 
-    router.post("/creat",async(req,res)=>{const answer = await Create_riddle(req.body); 
-    res.send(answer) })
+router.delete("/delete/:id", async (req, res) => {
+  const answer = await deleted(req.params.id);
+  res.send(answer)
+})
 
-    router.put("/update_riddle",async(req,res)=>{const answer = await update_riddle(req.body); 
-    res.send(answer) })
 
-    
+router.post("/creat", async (req, res) => {
+  const answer = await Create_riddle(req.body);
+  res.send(answer)
+})
+
+router.put("/update_riddle", async (req, res) => {
+  const answer = await update_riddle(req.body);
+  res.send(answer)
+})
+
+
 
 export default router;
